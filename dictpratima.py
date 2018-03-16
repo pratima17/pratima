f9f8df8909cb67fe541d9afba0000cb889527a7f
Pratima Gurung <pratima.gurung@stu.jefferson.kyschools.us>
2018-03-16 08:37:03 -0400

    firtcommit dict_assign

---
+++ dictpratima.py
@@ -0,0 +1,11 @@
+dict_pratimagurung = {
+    "aesthetic" : "concerned with beauty or the appreciation of beauty",
+    "grateful" : "feeling or showing an appreciation of kindness; thankful",
+    "hopeful" : "feeling or inspiring optimism about a future event",
+    "jaded" : "tired, bored, or lacking enthusiasm, typically after having had too much of something.",
+    "precocious" : "having developed certain abilities or proclivities at an earlier age than usua",
+    "euphemism" : "a mild or indirect word or expression substituted for one considered to be too harsh or blunt when referring to something unpleasant or embarrassing.",
+
+    }
+
+    
